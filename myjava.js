394418d186f7244920559f9a207942c837840cf2
svyazist2005 <svyazist2005@gmail.com>
2018-10-17 14:13:18 +0300

    basic logics added

--- myjava.js
+++ myjava.js
@@ -1,22 +1,56 @@
+
 new Vue({
   el:'#app',
   data:
-  {you:100,
-  monster:100,
+  {player:["100","100"],
   showMenu:false,
   history:[],
-  activities:["attacked","special attacked ","healed himself","gave up","started game"]},
+  activities:["attacked","special attacked ","healed himself","gave up","restarted game","started game","Win!","Loose"],
+  damage:[0,0]
+},
 
   methods:{
     showGameMenu:function(){
       this.showMenu=!this.showMenu;
     },
     reportActivity:function(action){
+      if(action==4|action==3){
       this.history.push("Player "+this.activities[action]);
+      }
+      else {
+        this.history.push("Player "+this.activities[action]+" "+this.damage[1]);
+      }
       console.log(this.history);
     },
     clearActivities:function(){
       this.history=[];
+      this.player=[10,10];
+      this.damage=[0,0];
+    },
+
+    attack:function(player){
+      this.damage[player]=Math.ceil(Math.random()*10);
+      this.player[player]-=this.damage[player]
+      console.log(this.damage[player]);
+    },
+
+    heal:function(player){
+      this.damage[player]=-Math.ceil(Math.random()*10);
+      this.player[player]-=this.damage[player]
+      console.log(this.damage[player]);
+    },
+
+
+    specialAttack:function(player){
+      this.damage[player]=Math.ceil(Math.random()*10)*2;
+      this.player[player]-=this.damage[player]
+      console.log(this.damage[player]);
+    },
+
+    playerAlive:function(player){
+      return this.player[player]>0;
     }
+
   }
+
 })
