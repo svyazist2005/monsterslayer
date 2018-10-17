1d66650358185d0b049fa6a418a34759ef8e5bd2
svyazist2005 <svyazist2005@gmail.com>
2018-10-17 12:50:37 +0300

    interface updated

--- myjava.js
+++ myjava.js
@@ -3,10 +3,20 @@ new Vue({
   data:
   {you:100,
   monster:100,
-  showMenu:false},
+  showMenu:false,
+  history:[],
+  activities:["attacked","special attacked ","healed himself","gave up","started game"]},
+
   methods:{
     showGameMenu:function(){
       this.showMenu=!this.showMenu;
+    },
+    reportActivity:function(action){
+      this.history.push("Player "+this.activities[action]);
+      console.log(this.history);
+    },
+    clearActivities:function(){
+      this.history=[];
     }
   }
 })
